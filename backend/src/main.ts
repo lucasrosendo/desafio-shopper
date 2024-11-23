3aa50a1d2205bbbb3fd834f645d88ce951b4485b
Lucas Rosendo <lucasrosendo91@gmail.com>
2024-11-23 16:14:09 -0300

    feat: config docker and dotenv

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: '../.env' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.BACKEND_PORT;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
