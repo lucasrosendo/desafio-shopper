e381b97fc1cfeb7bf19d662a4fe5686525db0440
Lucas Rosendo <lucasrosendo91@gmail.com>
2024-11-26 22:38:55 -0300

    fix: bug cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 dotenv.config({ path: '../.env' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors(corsOptions));
   const port = process.env.BACKEND_PORT || 4000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
